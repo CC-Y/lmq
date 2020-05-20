b8b023d313adc9f9ee82d20c5b5afbbdd77774e2
Qiuyi_Chen <cqy_666#163.com>
2020-05-20 14:25:15 +0800

    test update

---
+++ src/main/java/com/aliyun/openservices/lmq/example/demo/TestCommit.java
@@ -0,0 +1,11 @@
+package com.aliyun.openservices.lmq.example.demo;
+
+/**
+ * @Title :
+ * @Description :
+ * @Author :cqy
+ * @Since :2020/5/20 14:23
+ * @Version :1.1.0
+ */
+public class TestCommit {
+}
